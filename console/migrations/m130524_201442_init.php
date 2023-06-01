035c44baff93b3b0ce068cfee41c2c09fcbfd4e9
Paride Zavoli <paride.zavoli@outlook.com>
2023-06-01 13:37:57 +0200

    migrazione per creazione tabelle per autorizzazioni e utente

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -24,10 +24,62 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+        
+        $this->createTable('{{%ztrans}}', [
+            'idtrans' => $this->primaryKey(),
+            'nometrans' => $this->string(200)->notNull(),
+            'ultagg' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'utente' => $this->string(45)->notNull(),
+        ], $tableOptions);
+        
+        $this->createTable('{{%zgruppo}}', [
+            'idgruppo' => $this->primaryKey(),
+            'nomegruppo' => $this->string(200)->notNull(),
+            'ultagg' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'utente' => $this->string(45)->notNull(),
+        ], $tableOptions);
+        
+        $this->createTable('{{%zpermessi}}', [
+            'idpermessi' => $this->primaryKey(),
+            'idtrans' => $this->integer()->notNull(),
+            'idgruppo' => $this->integer()->notNull(),
+            'permesso' => $this->string(45)->notNull(),
+            'ultagg' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'utente' => $this->string(45)->notNull(),
+        ], $tableOptions);
+        $this->addForeignKey('fk_permessi_trans', '{{%zpermessi}}', ['idtrans'], '{{%ztrans}}', ['idtrans']);
+        $this->addForeignKey('fk_permessi_gruppo', '{{%zpermessi}}', ['idgruppo'], '{{%zgruppo}}', ['idgruppo']);
+
+        $this->createTable('{{%zutgr}}', [
+            'idutgr' => $this->primaryKey(),
+            'id' => $this->integer()->notNull(),
+            'idgruppo' => $this->integer()->notNull(),
+            'ultagg' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'utente' => $this->string(45)->notNull(),
+        ], $tableOptions);
+        $this->addForeignKey('fk_utgr_user', '{{%zutgr}}', ['id'], '{{%user}}', ['id']);
+        $this->addForeignKey('fk_utgr_gruppo', '{{%zutgr}}', ['idgruppo'], '{{%zgruppo}}', ['idgruppo']);
+        
+        $this->createTable('{{%profilo}}', [
+            'idprofilo' => $this->primaryKey(),
+            'id' => $this->integer()->notNull(),
+            'Cognome' => $this->string(100),
+            'Nome' => $this->string(100)->notNull(),
+            'Nascita' => $this->date(),
+            'NascitaAnno' => $this->integer()->notNull()->defaultValue(0),
+            'ultagg' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'utente' => $this->string(45)->notNull(),
+        ], $tableOptions);
+        $this->addForeignKey('fk_profilo_user', '{{%profilo}}', ['id'], '{{%user}}', ['id']);        
     }
 
     public function down()
     {
         $this->dropTable('{{%user}}');
+        $this->dropTable('{{%ztrans}}');
+        $this->dropTable('{{%zgruppo}}');
+        $this->dropTable('{{%zpermessi}}');
+        $this->dropTable('{{%zutgr}}');
+        $this->dropTable('{{%profilo}}');        
     }
 }
