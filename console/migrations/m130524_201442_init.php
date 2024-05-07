631e2ae2155bd4e8d264c6abd1be31d72a4ebfb0
Paride Zavoli <61480658+pzavoli71@users.noreply.github.com>
2024-05-07 14:03:16 +0200

    Aggiunta tabella sessione su db

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -70,7 +70,17 @@ class m130524_201442_init extends Migration
             'ultagg' => $this->datetime()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
             'utente' => $this->string(45)->notNull(),
         ], $tableOptions);
-        $this->addForeignKey('fk_profilo_user', '{{%profilo}}', ['id'], '{{%user}}', ['id']);        
+        $this->addForeignKey('fk_profilo_user', '{{%profilo}}', ['id'], '{{%user}}', ['id']);     
+
+        $this->createTable('session', [
+            'id' => $this->char(40)->notNull(),
+            'expire' => $this->integer(),
+            'data' => $this->binary(),
+            'user_id' => $this->integer(),
+            'browser_platform' => $this->char(200)->notNull(),
+        ]);
+
+        $this->addPrimaryKey('session_pk', 'session', 'id');              
     }
 
     public function down()
