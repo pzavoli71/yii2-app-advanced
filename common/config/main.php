ed1e977ae662a4a9a370ea52cee5a88ee4cecb70
Paride Zavoli <61480658+pzavoli71@users.noreply.github.com>
2024-05-07 14:01:53 +0200

    Aggiunta gestione sessioni su db

--- common/config/main.php
+++ common/config/main.php
@@ -39,6 +39,23 @@ return [
             'class' => 'yii\rbac\DbManager',
             // uncomment if you want to cache RBAC items hierarchy
             // 'cache' => 'cache',
+        ],   
+        'session' => [
+            'class' => 'yii\web\DbSession',
+            'writeCallback' => function ($session) {
+                $user_browser = null;
+                if (!Yii::$app->user->isGuest) {
+                    $browser = new \BrowserDetection();
+                    $user_browser = "{$browser->getName()}-{$browser->getPlatform()}" . ($browser->is64bitPlatform() ? "(x64)" : "(x86)") . ($browser->isMobile() ? "-Mobile" : "-Desktop");
+                }
+                return [
+                    'user_id' => Yii::$app->user->id,
+                    //'last_write' => new \yii\db\Expression('NOW()'),
+                    'browser_platform' => $user_browser
+                ];
+            }
+            // 'db' => 'mydb',  // the application component ID of the DB connection. Defaults to 'db'.
+            // 'sessionTable' => 'my_session', // session table name. Defaults to 'session'.
         ],        
     ],
    'modules'    => [
