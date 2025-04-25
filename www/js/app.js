396f65b6e4b7d20fcb962d85f92cb177e05dd680
Paride Zavoli <61480658+pzavoli71@users.noreply.github.com>
2025-04-25 15:49:14 +0200

    La form si apre al centro

--- www/js/app.js
+++ www/js/app.js
@@ -394,7 +394,7 @@ function AppGlob() {
                     autoOpen: true,
                     modal:false,
                     title:title,
-                    position:{ my: 'top', at: 'top+100', of: window.top },
+                    position:{ my: "center", at: "center", of: window} },
                             width: width,
                             show: {
                             effect: "blind",
