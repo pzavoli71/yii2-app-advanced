8ffa52ec042817cd2f75bdf5acdf85fbb17e8a3b
Paride Zavoli <61480658+pzavoli71@users.noreply.github.com>
2026-07-21 17:27:46 +0200

    Revise homepage for educational goal management
    Updated the homepage to reflect educational goal management features and user access options.

--- frontend/views/site/home.php
+++ frontend/views/site/home.php
@@ -2,51 +2,53 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+$this->title = 'Gestione obiettivi scolastici';
+$isGuest = Yii::$app->user->isGuest;
 ?>
-<div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+<div class="os-home">
+
+    <section class="os-hero">
+        <span class="os-eyebrow"><i class="fas fa-graduation-cap"></i>&nbsp; Piattaforma didattica</span>
+        <h1>Gestione obiettivi scolastici</h1>
+        <p class="lead">
+            Organizza obiettivi, materie e verifiche in un unico posto. Monitora i
+            progressi degli studenti e allena le competenze con i quiz interattivi.
+        </p>
+        <div class="os-cta">
+            <?php if ($isGuest): ?>
+                <?= Html::a('<i class="fas fa-sign-in-alt"></i>&nbsp; Accedi', Url::to(['/site/login']), ['class' => 'btn btn-light']) ?>
+                <?= Html::a('<i class="fas fa-user-plus"></i>&nbsp; Registrati', Url::to(['/site/signup']), ['class' => 'btn btn-ghost']) ?>
+            <?php else: ?>
+                <?= Html::a('<i class="fas fa-bullseye"></i>&nbsp; Vai agli obiettivi', Url::to(['/busy/obiettivo/index']), ['class' => 'btn btn-light']) ?>
+                <?= Html::a('<i class="fas fa-question-circle"></i>&nbsp; Quiz', Url::to(['/patente/quiz/index']), ['class' => 'btn btn-ghost']) ?>
+            <?php endif; ?>
         </div>
-    </div>
+    </section>
 
-    <div class="body-content">
+    <section class="os-features">
+        <article class="os-feature-card">
+            <div class="os-feature-icon"><i class="fas fa-bullseye"></i></div>
+            <h3>Obiettivi</h3>
+            <p>Definisci e segui gli obiettivi didattici, con permessi e stato di avanzamento sempre aggiornati.</p>
+        </article>
+        <article class="os-feature-card">
+            <div class="os-feature-icon"><i class="fas fa-book"></i></div>
+            <h3>Materie</h3>
+            <p>Gestisci le materie associate a ogni utente e organizza i contenuti per disciplina.</p>
+        </article>
+        <article class="os-feature-card">
+            <div class="os-feature-icon"><i class="fas fa-question-circle"></i></div>
+            <h3>Quiz</h3>
+            <p>Allena le competenze con quiz interattivi e tieni traccia delle domande da rivedere.</p>
+        </article>
+        <article class="os-feature-card">
+            <div class="os-feature-icon"><i class="fas fa-chart-line"></i></div>
+            <h3>Prodotti finanziari</h3>
+            <p>Consulta e confronta i prodotti finanziari e gli investimenti collegati all'attività.</p>
+        </article>
+    </section>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
 </div>
